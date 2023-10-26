c99dfe7e9ba4e4df89201597c908f5337fd22ab2
artick95 <74319852+artick95@users.noreply.github.com>
2023-10-26 21:39:27 +0100

    working code v2

--- csvCleaner.py
+++ csvCleaner.py
@@ -1,47 +1,55 @@
 import pandas as pd
 import re
 
-def clean_emails_from_csv():
+def clean_emails_with_name_prioritization():
+    # Load the popular names CSV
+    names_df = pd.read_csv("/Users/artick/Documents/Scripts/CsvCleaners/Popular Names by Country - USA Names.csvG")
+    popular_names = names_df["Male Name"].str.lower().tolist() + names_df["Female Name"].str.lower().tolist()
+    
     # Load CSV file with dtype=str
     youtubers_df = pd.read_csv("/Users/artick/Documents/Scripts/CsvCleaners/youtubersTest.csv", dtype=str)
-
-    # Drop any unnamed columns (like "Unnamed: 2", "Unnamed: 3", etc.)
     youtubers_df = youtubers_df.filter(regex='^(?!Unnamed)')
-
-    # Convert the email column to string type
     youtubers_df['email'] = youtubers_df['email'].astype(str)
-
-    # Regular expression pattern for matching email format
+    
     email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-
-    # Define a function to check if an email is valid
     def is_valid_email(email):
         return bool(re.match(email_pattern, email))
-
-    # Filter out invalid emails using the is_valid_email function
     youtubers_df = youtubers_df[youtubers_df['email'].apply(is_valid_email)]
-
-    # Group by website and get a list of emails associated with each website
     grouped = youtubers_df.groupby('website')['email'].apply(list).reset_index()
-
-    # Convert the list of emails into separate columns
     email_columns = grouped['email'].apply(pd.Series)
-
-    # Rename the columns to "email1", "email2", etc.
-    email_columns.columns = [f'email{i+1}' for i in range(email_columns.shape[1])]
-
-    # Concatenate the website and email columns
-    final_df = pd.concat([grouped['website'], email_columns], axis=1)
-
-    # Patterns for cleaning criteria
+    
+    # Check if email_columns is empty
+    if email_columns.empty:
+        print("No valid emails found.")
+        return
+    
+    # Limit to max 6 email columns
+    max_cols = 6
+    if email_columns.shape[1] > max_cols:
+        email_columns = email_columns.iloc[:, :max_cols]
+        
+    email_columns.columns = ['email' + str(i+1) for i in range(email_columns.shape[1])]
+    
+    def best_email(emails):
+        if not emails:
+            return None
+        emails = sorted(emails, key=lambda email: (
+            not any(name in email for name in popular_names),
+            "info" in email,
+            "contact" in email,
+            "support" in email,
+            "hello" in email,
+            "admin" in email,
+            email
+        ))
+        return emails[0]
+    
     patterns_to_remove = [
         "domain.com", "sentry.io", "wixpress", "ingest", "example.com", ".edu",
         r"\.(jpg|jpeg|png|gif|bmp|webp|svg|mp4|avi|flv|mkv|mov)$", 
         r"@\d+\.\d+x"
     ]
     starting_patterns_to_remove = ["privacy", "terms"]
-
-    # Define a function to check if an email should be removed based on the patterns
     def should_remove_email(email):
         if email is None or not isinstance(email, str):
             return False
@@ -50,14 +58,19 @@ def clean_emails_from_csv():
         if any(email.lower().startswith(pattern) for pattern in starting_patterns_to_remove):
             return True
         return False
-
-    # Apply the cleaning function to each email column and replace invalid emails with NaN
-    for col in final_df.columns:
-        if col.startswith("email"):
-            final_df[col] = final_df[col].where(~final_df[col].apply(should_remove_email), other=None)
-
-    # Save the cleaned dataframe to the specified output path
+    
+    for col in email_columns.columns:
+        email_columns[col] = email_columns[col].where(~email_columns[col].apply(should_remove_email), other=None)
+    
+    final_df = pd.concat([grouped['website'], email_columns], axis=1)
+    final_df['emailBest'] = final_df.apply(lambda row: best_email(row.filter(like="email").dropna().tolist()), axis=1)
+    
+    # Reorder columns to make emailBest the second column
+    cols = final_df.columns.tolist()
+    cols.insert(1, cols.pop(cols.index('emailBest')))
+    final_df = final_df[cols]
+    
     final_df.to_csv("cleaned_output_file.csv", index=False)
+    print("Email cleaning and prioritization complete. Output saved to cleaned_output_file.csv.")
 
-# Call the function
-clean_emails_from_csv()
+clean_emails_with_name_prioritization()
